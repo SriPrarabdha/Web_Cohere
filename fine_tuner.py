70d34f461892ac7e942acfcb0304786b2c6a4a80
Prarabdha <prabodhkumarsrivastav70@gmail.com>
2023-07-22 00:19:15 +0530

    data_extractor bug resolved

--- fine_tuner.py
+++ fine_tuner.py
@@ -1,3 +1,13 @@
 import cohere
-co = cohere.Client('xCRrVzZjuPM5HN6WFKM1eykBBwHMezrMhaQ0AaD7)
-co
+from data_extractor import loader
+
+co = cohere.Client('xCRrVzZjuPM5HN6WFKM1eykBBwHMezrMhaQ0AaD7')
+
+data = loader.load()
+
+dataset= []
+for i in range(len(data)):
+    dataset.append({
+        "prompt" : data[i].metadata["title"] + data[0].metadata["description"],
+        "completion" : data[i].page_content
+    })
