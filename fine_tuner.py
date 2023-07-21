314e0ca45b0992dfcf32e7c5e34fc6b9bf25ff24
Prarabdha <prabodhkumarsrivastav70@gmail.com>
2023-07-22 00:45:55 +0530

    finetuner

--- fine_tuner.py
+++ fine_tuner.py
@@ -7,7 +7,19 @@ data = loader.load()
 
 dataset= []
 for i in range(len(data)):
-    dataset.append({
-        "prompt" : data[i].metadata["title"] + data[0].metadata["description"],
-        "completion" : data[i].page_content
-    })
+    res = co.summarize(
+    text=data[i].page_content,
+    model="summarize-xlarge",
+    length="long",
+    temperature=0.3,
+    additional_command="summarize it in form of 5 question that can be a high quality prompt to a Large Language model")
+
+    prompts = res.summary.splitlines()
+    for prompt in prompts:
+        prompt = prompt[2:]
+        dataset.append({
+            "prompt" : data[i].metadata["title"] + data[i].metadata["description"] + prompt,
+            "completion" : data[i].page_content
+        })
+
+print(dataset)
